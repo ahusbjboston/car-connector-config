81f2bb52d304b56d5bf1ef87aa71b3e1c9c1240b
ahusbjboston <56374603+ahusbjboston@users.noreply.github.com>
2020-07-29 17:52:25 -0400

    Delete util.py

--- util.py
+++
@@ -1,7 +0,0 @@
-import os, json
-
-def patch(type, name, data):
-    json_data = json.dumps(data)
-    command = "kubectl patch %s %s --type merge --patch '%s'" % (type, name, json_data)
-    print(command)
-    os.system(command)
