52103f3d6ebd4d015f94eed284ed192a173636e5
ahusbjboston <56374603+ahusbjboston@users.noreply.github.com>
2020-07-30 07:36:24 -0400

    Update api_v1.py

--- src/api_v1.py
+++ src/api_v1.py
@@ -1,6 +1,6 @@
 from flask import request, Blueprint, Response
 import entitlement, jsonpickle
-from jwtcheker import requires_auth
+# from jwtcheker import requires_auth
 from connector_config import ConnectorConfig
 
 from errors import error_handler
@@ -12,7 +12,7 @@ api = Blueprint('route', __name__)
 
 @api.route('/connectorConfigs',  methods=['GET'])
 @error_handler
-@requires_auth(required_access_level=entitlement.readOnlyAccess)
+# @requires_auth(required_access_level=entitlement.readOnlyAccess)
 def list():
     res = context().persister.list()
     return Response(jsonpickle.encode(res, unpicklable=False), status=200, mimetype='application/json')
@@ -20,14 +20,14 @@ def list():
 
 @api.route('/connectorConfigs/<cronJobName>', methods=['GET']) 
 @error_handler
-@requires_auth(required_access_level=entitlement.readOnlyAccess)
+# @requires_auth(required_access_level=entitlement.readOnlyAccess)
 def get(cronJobName):
     cronjob = context().persister.get(cronJobName)
     return Response(jsonpickle.encode(cronjob, unpicklable=False), status=200, mimetype='application/json')
 
 
 @api.route('/connectorConfigs', methods=['POST'])
-#@error_handler
+@error_handler
 #@requires_auth(required_access_level=entitlement.readWriteAccess)
 def create():
     print (request.get_json())
@@ -37,7 +37,7 @@ def create():
 
 @api.route('/connectorConfigs/<cronJobName>', methods=['POST'])
 @error_handler
-@requires_auth(required_access_level=entitlement.readWriteAccess)
+# @requires_auth(required_access_level=entitlement.readWriteAccess)
 def update(cronJobName):
     json_data = request.get_json()
     if json_data.get('name') != cronJobName:
@@ -48,7 +48,7 @@ def update(cronJobName):
 
 @api.route('/connectorConfigs/<cronJobName>', methods=['DELETE'])
 @error_handler
-@requires_auth(required_access_level=entitlement.readWriteAccess)
+# @requires_auth(required_access_level=entitlement.readWriteAccess)
 def delete(cronJobName):
     context().persister.delete(cronJobName)
     return Response('Success', 200, {'Content-Type': 'text/plain'})
