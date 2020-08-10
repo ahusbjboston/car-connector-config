7e8467833be08cc491eb3c1af34dbb1c9f404765
li tan <li.tan1@ibm.com>
2020-08-10 13:02:56 -0400

    update test case

--- src/entitlement.py
+++ src/entitlement.py
@@ -33,38 +33,32 @@ def entitlementCheck(jwtToken, jwtdecodedToken, required_access_level):
 
     entitlements_response= jwtdecodedToken['details']['userPrivileges']
     if entitlements_response :
-            for permission in required_access_level:
-                try: 
-                    entitlements_response.index(permission)
-                except ValueError: raise errors.AuthError(f'Authorization faild: Blocking unauthorized access due to missing role {permission}', 401)
-                else:
-                    raise errors.AuthError('Failed to get entitlements. Cannot get the entitlements.', 401)
+        for permission in required_access_level:
+            try: 
+                entitlements_response.index(permission)
+            except ValueError: raise errors.AuthError(f'Authorization faild: Blocking unauthorized access due to missing role {permission}', 401)
+    else:
+        raise errors.AuthError('Failed to get entitlements. Cannot get the entitlements.', 401)
             
-    except (ContentDecodingError, Timeout, InvalidURL, ContentDecodingError) as e:
-        raise errors.AuthError(f'Failed to get entitlements.', 401)
-
-
-    # """
-    try:
-        response = requests.post(
-            os.environ.get('ENTITLEMENTS_API') + '/application',
-            headers = {
-                'accept': 'application/json',
-                'content-type': 'application/json',
-                'Authorization': f'Bearer {jwtToken}'
-                },
-            timeout = 10000,
-            json = True,
-            data = json.dumps(body)
-        )
-        entitlements_response = response.json()
-        if entitlements_response and entitlements_response['entitlements'][0]:
-            for permission in required_access_level:
-                try: entitlements_response['entitlements'][0]['permissions'].index(permission)
-                except ValueError: raise errors.AuthError(f'Authorization faild: Blocking unauthorized access due to missing role {permission}', 401)
-        else:
-            raise errors.AuthError('Failed to get entitlements. Cannot get the entitlements.', 401)
+    # try:
+    #     response = requests.post(
+    #         os.environ.get('ENTITLEMENTS_API') + '/application',
+    #         headers = {
+    #             'accept': 'application/json',
+    #             'content-type': 'application/json',
+    #             'Authorization': f'Bearer {jwtToken}'
+    #             },
+    #         timeout = 10000,
+    #         json = True,
+    #         data = json.dumps(body)
+    #     )
+    #     entitlements_response = response.json()
+    #     if entitlements_response and entitlements_response['entitlements'][0]:
+    #         for permission in required_access_level:
+    #             try: entitlements_response['entitlements'][0]['permissions'].index(permission)
+    #             except ValueError: raise errors.AuthError(f'Authorization faild: Blocking unauthorized access due to missing role {permission}', 401)
+    #     else:
+    #         raise errors.AuthError('Failed to get entitlements. Cannot get the entitlements.', 401)
             
-    except (ContentDecodingError, Timeout, InvalidURL, ContentDecodingError) as e:
-        raise errors.AuthError(f'Failed to get entitlements.', 401)
-    """
+    # except (ContentDecodingError, Timeout, InvalidURL, ContentDecodingError) as e:
+    #     raise errors.AuthError(f'Failed to get entitlements.', 401)
